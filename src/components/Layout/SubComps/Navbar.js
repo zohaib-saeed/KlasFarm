a4959e0fa331a8d258c3730ac21cbdbc3c29046f
zohaibzyebi <zyebi553@gmail.com>
2023-04-15 03:25:44 +0500

    fix: navbar dropdown

--- src/components/Layout/SubComps/Navbar.js
+++ src/components/Layout/SubComps/Navbar.js
@@ -44,126 +44,125 @@ const Navbar = () => {
       {/* Menu => Desktop (Above 1024px) */}
       <div className="w-full max-w-[1440px] px-6 py-[6px] lg:py-[16px] hidden lg:flex items-center justify-between gap-4 sxl:gap-5 mxl:gap-6 ">
         {/* Logo  */}
-        <Link href="/">
-          <div className="w-[170px] sxl:w-[190px] mxl:w-[210px] xl:w-[220px] h-[90px] mxl:h-[120px] xl:h-[125px] flex items-center justify-center relative transition-all ">
-            <Image
-              src="/images/logo-main.svg"
-              alt=""
-              fill
-              className="object-contain"
-            />
-          </div>
+        <Link
+          href="/"
+          className="w-[170px] sxl:w-[190px] mxl:w-[210px] xl:w-[220px] h-[90px] mxl:h-[120px] xl:h-[125px] flex items-center justify-center relative transition-all "
+        >
+          <Image
+            src="/images/logo-main.svg"
+            alt=""
+            fill
+            className="object-contain"
+          />
         </Link>
         {/* Nav Links  */}
         <div className="w-full flex items-center justify-center gap-3 sxl:gap-4">
           {/* About Us  */}
-          <Link href="/about-us">
-            <div
-              className={`text-[16px] hover:text-fireBush hover:font-normal  sxl:text-[18px] mxl:text-[20px] xl:text-[25px]  font-normal cursor-pointer hover-link relative transition-all duration-300`}
-            >
-              O nás
-            </div>
+          <Link
+            href="/about-us"
+            className={`text-[16px] hover:text-fireBush hover:font-normal  sxl:text-[18px] mxl:text-[20px] xl:text-[25px]  font-normal cursor-pointer hover-link relative transition-all duration-300`}
+          >
+            O nás
           </Link>
           {/* We offer  */}
-          <Link href="/we-offer">
-            <div
-              className={`we-offer hover:text-fireBush hover:font-normal  text-[16px] relative  sxl:text-[18px] mxl:text-[20px] xl:text-[25px] font-normal cursor-pointer transition-all  duration-300 hover-link`}
-            >
+          <div
+            className={`we-offer hover:text-fireBush hover:font-normal  text-[16px] relative  sxl:text-[18px] mxl:text-[20px] xl:text-[25px] font-normal cursor-pointer transition-all  duration-300 hover-link`}
+          >
+            <Link href="/we-offer" className="w-full text-left">
               Ponúkame
-              <div
-                onClick={preventDefaultClick}
-                className="we-offer-dropdown w-[260px] z-[10] hidden flex-col items-start justify-start gap-[3px] bg-davyGrey px-3 py-3 text-white text-[14px] absolute top-[22px] sxl:top-[25px] mxl:top-[28px] xl:top-[33px] left-0 rounded-b-lg transition-all"
-              >
-                <Link href="/we-offer#post-collection">Pozberová linka</Link>
-                <Link href="/we-offer#seed-production">Výroba osív</Link>
-                <Link href="/we-offer#products-sale">
-                  Predaj priemyselných hnojív...
-                </Link>
-                <Link href="/we-offer#cattle-sale">
-                  Predaj hovädzieho dobytka
-                </Link>
-                <Link href="/we-offer#sheep-milk-sale">
-                  Predaj ovčieho mlieka
-                </Link>
-                <Link href="/we-offer#agr-commodities">
-                  Predaj a výkup agrokomodít
-                </Link>
-                <Link href="/we-offer#ecological-agriculture">
-                  Ekologické poľnohospodárstvo
-                </Link>
-                <Link href="/we-offer#mechanization">
-                  Poskytovanie mechanizačných...
-                </Link>
-              </div>
-            </div>
-          </Link>
-          {/* Contacts  */}
-          <Link href="/contacts">
+            </Link>
             <div
-              className={`text-[16px]  hover:text-fireBush hover:font-normal  sxl:text-[18px] mxl:text-[20px] xl:text-[25px]  font-normal cursor-pointer relative hover-link transition-all duration-300`}
+              onClick={preventDefaultClick}
+              className="we-offer-dropdown w-[260px] z-[10] hidden flex-col items-start justify-start gap-[3px] bg-davyGrey px-3 py-3 text-white text-[14px] absolute top-[22px] sxl:top-[25px] mxl:top-[28px] xl:top-[33px] left-0 rounded-b-lg transition-all"
             >
-              Kontakty
+              <Link href="/we-offer#post-collection">Pozberová linka</Link>
+              <Link href="/we-offer#seed-production">Výroba osív</Link>
+              <Link href="/we-offer#products-sale">
+                Predaj priemyselných hnojív...
+              </Link>
+              <Link href="/we-offer#cattle-sale">
+                Predaj hovädzieho dobytka
+              </Link>
+              <Link href="/we-offer#sheep-milk-sale">
+                Predaj ovčieho mlieka
+              </Link>
+              <Link href="/we-offer#agr-commodities">
+                Predaj a výkup agrokomodít
+              </Link>
+              <Link href="/we-offer#ecological-agriculture">
+                Ekologické poľnohospodárstvo
+              </Link>
+              <Link href="/we-offer#mechanization">
+                Poskytovanie mechanizačných...
+              </Link>
             </div>
+          </div>
+          {/* Contacts  */}
+          <Link
+            href="/contacts"
+            className={`text-[16px]  hover:text-fireBush hover:font-normal  sxl:text-[18px] mxl:text-[20px] xl:text-[25px]  font-normal cursor-pointer relative hover-link transition-all duration-300`}
+          >
+            Kontakty
           </Link>
           {/* Gallery  */}
-          <Link href="/gallery">
-            <div
-              className={`text-[16px] hover:text-fireBush hover:font-normal sxl:text-[18px] mxl:text-[20px] xl:text-[25px] font-normal cursor-pointer   relative hover-link transition-all duration-300`}
-            >
-              Galéria
-            </div>
+          <Link
+            href="/gallery"
+            className={`text-[16px] hover:text-fireBush hover:font-normal sxl:text-[18px] mxl:text-[20px] xl:text-[25px] font-normal cursor-pointer   relative hover-link transition-all duration-300`}
+          >
+            Galéria
           </Link>
           {/* Corn Maze  */}
-          <Link href="/corn-maze">
-            <div
-              className={`text-[16px] hover:text-fireBush hover:font-normal  sxl:text-[18px] mxl:text-[20px] xl:text-[25px]  font-normal cursor-pointer relative hover-link transition-all duration-300`}
-            >
-              Kukuričné bludisko
-            </div>
+          <Link
+            href="/corn-maze"
+            className={`text-[16px] hover:text-fireBush hover:font-normal  sxl:text-[18px] mxl:text-[20px] xl:text-[25px]  font-normal cursor-pointer relative hover-link transition-all duration-300`}
+          >
+            Kukuričné bludisko
           </Link>
           {/* News  */}
-          <Link href="/news">
-            <div
-              className={`text-[16px] hover:text-fireBush hover:font-normal sxl:text-[18px] mxl:text-[20px] xl:text-[25px] font-normal cursor-pointer relative hover-link transition-all duration-300`}
-            >
-              Aktuality
-            </div>
+          <Link
+            href="/news"
+            className={`text-[16px] hover:text-fireBush hover:font-normal sxl:text-[18px] mxl:text-[20px] xl:text-[25px] font-normal cursor-pointer relative hover-link transition-all duration-300`}
+          >
+            Aktuality
           </Link>
         </div>
         {/* Social Links  */}
         <div className="flex items-center justify-end gap-[4px]">
           {/* Facebook  */}
-          <Link href="https://web.facebook.com/klasfarm1?_rdc=1&_rdr">
-            <div className="w-[35px] xl:w-[40px] h-[35px] xl:h-[40px] flex items-center justify-center relative">
-              <Image
-                src="/icons/social/fb.svg"
-                alt=""
-                fill
-                className="object-contain"
-              />
-            </div>
+          <Link
+            href="https://web.facebook.com/klasfarm1?_rdc=1&_rdr"
+            className="w-[35px] xl:w-[40px] h-[35px] xl:h-[40px] flex items-center justify-center relative"
+          >
+            <Image
+              src="/icons/social/fb.svg"
+              alt=""
+              fill
+              className="object-contain"
+            />
           </Link>
           {/* Instagram  */}
-          <Link href="https://www.instagram.com/klas_farm_/">
-            <div className="w-[35px] xl:w-[40px] h-[35px] xl:h-[40px] flex items-center justify-center relative">
-              <Image
-                src="/icons/social/ig.svg"
-                alt=""
-                fill
-                className="object-contain"
-              />
-            </div>
+          <Link
+            href="https://www.instagram.com/klas_farm_/"
+            className="w-[35px] xl:w-[40px] h-[35px] xl:h-[40px] flex items-center justify-center relative"
+          >
+            <Image
+              src="/icons/social/ig.svg"
+              alt=""
+              fill
+              className="object-contain"
+            />
           </Link>
           {/* Youtube  */}
-          <Link href="https://www.youtube.com/channel/UCQ6DTc6XwIql9JTz5XlYqGw">
-            <div className="w-[35px] xl:w-[40px] h-[35px] xl:h-[40px] flex items-center justify-center relative">
-              <Image
-                src="/icons/social/ytb.svg"
-                alt=""
-                fill
-                className="object-contain"
-              />
-            </div>
+          <Link
+            href="https://www.youtube.com/channel/UCQ6DTc6XwIql9JTz5XlYqGw"
+            className="w-[35px] xl:w-[40px] h-[35px] xl:h-[40px] flex items-center justify-center relative"
+          >
+            <Image
+              src="/icons/social/ytb.svg"
+              alt=""
+              fill
+              className="object-contain"
+            />
           </Link>
           {/* Find  */}
           <div className="w-[35px] xl:w-[40px] h-[35px] xl:h-[40px] cursor-pointer flex items-center justify-center relative">
@@ -179,15 +178,16 @@ const Navbar = () => {
       {/* Menu => Small Screens (Below 1024px) */}
       <div className="w-full flex lg:hidden items-center justify-between px-2 xs:px-3 sm:px-4 md:px-5 py-[8px]">
         {/* Logo  */}
-        <Link href="/">
-          <div className="w-[170px] h-[90px]   flex items-center justify-center relative  trasition-all">
-            <Image
-              src="/images/logo-main.svg"
-              alt=""
-              fill
-              className="object-contain"
-            />
-          </div>
+        <Link
+          href="/"
+          className="w-[170px] h-[90px]   flex items-center justify-center relative  trasition-all"
+        >
+          <Image
+            src="/images/logo-main.svg"
+            alt=""
+            fill
+            className="object-contain"
+          />
         </Link>
         <div className="flex items-center justify-end gap-2 md:gap-3">
           {/* Find  */}
