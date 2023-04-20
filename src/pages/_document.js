4a461b5e0dd6a673e7c8f15ccb63815468a18171
zohaibzyebi <zyebi553@gmail.com>
2023-04-20 16:13:01 +0500

    fix: font file issue

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -11,13 +11,13 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
             rel="stylesheet"
           />
-          <style>{`
+          {/* <style>{`
             @font-face {
               font-family: 'Mistral';
               src: url('/fonts/MISTRAL.TTF') format('truetype');
               font-display: swap;
             }
-          `}</style>
+          `}</style> */}
         </Head>
         <body>
           <Main />
